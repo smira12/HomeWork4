91152d29d2da63a18df95994b826d36f6c82683a
smira12 <m1ronsa89@gmail.com>
2022-12-12 00:54:51 +0300

    Add Task №25

---
+++ Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while (!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели не число! Введите число");
+        }
+    }
+    return result;
+}
+
+void PrintPowerNumber(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine($"А в натуральной степени В равно: {result}");
+}
+
+int a = GetNumber("Введите число А: ");
+int b = GetNumber("Введите число B: ");
+PrintPowerNumber(a, b);
