40ea9ff70767c45d869d9a573df972e62419e441
smira12 <m1ronsa89@gmail.com>
2022-12-13 02:20:31 +0300

    Add Task №29

---
+++ Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+
+Console.Write("Введите количество элементов массива: ");
+int elements = int.Parse(Console.ReadLine() ?? "");
+
+int[] myArray = new int[elements];
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.Write($"Введите элемент массива под индексом {i}: ");
+    myArray[i] = int.Parse(Console.ReadLine() ?? "");
+}
+Console.Write($"Вывод массива: ");
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.Write(myArray[i]);
+    if (i < myArray.Length - 1)
+        Console.Write(", ");
+}
+Console.WriteLine();
